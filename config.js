e8d8b10f637e5bd24480edbb8849f3038b4b5632
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-24 19:14:08 +0700

    Update mangaUrls in config.js

--- config.js
+++ config.js
@@ -15,6 +15,6 @@ export const config = {
     // 'https://bbato.com/read/slug/chapter_slug'
   ], 
   mangaUrls: [
-    "https://cgbum.com/komik/payback-samk-fujokingcgbum"
+    "https://cgbum.com/komik/love-interest-zonecgbum"
   ]
 };
