aaefcf8b858464707d233287b6632b7006d07dbf
Hamzah <hamzahhaz740@gmail.com>
2026-08-04 20:51:07 +0700

    Last Sync: 2026-08-04 20:51 (Mobile)

--- config.js
+++ config.js
@@ -15,6 +15,6 @@ export const config = {
     // 'https://bbato.com/read/slug/chapter_slug'
   ], 
   mangaUrls: [
-    "https://cgbum.com/komik/honbulcgbum"
+    "https://cgbum.com/komik/jinxcgbum"
   ]
 };
